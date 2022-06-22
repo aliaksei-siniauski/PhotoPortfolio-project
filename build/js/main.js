5286cf691f8306a60882119722aeb5d63aa46be0
github-username <mail@gmail.com>
2022-06-23 01:21:32 +0200

    feat: add adaptive menu

---
+++ build/js/main.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".burger"),t=document.querySelector(".header__nav"),n=document.querySelector(".nav-list");document.querySelectorAll(".nav-list__item");e.addEventListener("click",(()=>{e.classList.toggle("open-menu"),t.classList.toggle("open-menu"),n.classList.toggle("open-menu")}));
+//# sourceMappingURL=main.js.map
